10f1f7c6ab8b0a992aa7cc32c70b21a1a91e2bab
Patrick Freed <patrick.freed@mongodb.com>
2019-05-02 13:32:21 -0400

    TOOLS-2285 / TOOLS-2132 Improve read preference handling

--- db/read_preferences_test.go
+++ db/read_preferences_test.go
@@ -34,20 +34,43 @@ func TestNewReadPreference(t *testing.T) {
 			So(pref.Mode(), ShouldEqual, readpref.PrimaryMode)
 		})
 
+		Convey("Specifying just the mode in the command line should set it correctly", func() {
+			rp := "primary"
+			pref, err := NewReadPreference(rp, nil)
+			So(err, ShouldBeNil)
+			So(pref.Mode(), ShouldEqual, readpref.PrimaryMode)
+
+			rp = "secondary"
+			pref, err = NewReadPreference(rp, nil)
+			So(err, ShouldBeNil)
+			So(pref.Mode(), ShouldEqual, readpref.SecondaryMode)
+
+			rp = "nearest"
+			pref, err = NewReadPreference(rp, nil)
+			So(err, ShouldBeNil)
+			So(pref.Mode(), ShouldEqual, readpref.NearestMode)
+		})
+
 		Convey("Specifying a read preference only on the command line should set it correctly", func() {
-			rp := "{\"mode\": \"secondary\", \"tags\": {\"foo\": \"bar\"}}"
+			rp := "{\"mode\": \"secondary\", \"tagSets\": [{\"foo\": \"bar\"}], maxStalenessSeconds: 123}"
 			pref, err := NewReadPreference(rp, nil)
 			So(err, ShouldBeNil)
 			So(pref.Mode(), ShouldEqual, readpref.SecondaryMode)
+
 			tagSets := pref.TagSets()
 			So(len(tagSets), ShouldEqual, 1)
 			So(tagSets[0], ShouldResemble, tag.Set{tag.Tag{Name: "foo", Value: "bar"}})
+
+			maxStaleness, set := pref.MaxStaleness()
+			So(set, ShouldBeTrue)
+			So(maxStaleness, ShouldEqual, 123 * time.Second)
 		})
 
 		Convey("Specifying a read preference only in the URI should set it correctly", func() {
 			pref, err := NewReadPreference("", cs)
 			So(err, ShouldBeNil)
 			So(pref.Mode(), ShouldEqual, readpref.SecondaryMode)
+
 			tagSets := pref.TagSets()
 			So(len(tagSets), ShouldEqual, 1)
 			So(tagSets[0], ShouldResemble, tag.Set{tag.Tag{Name: "foo", Value: "bar"}})
@@ -58,13 +81,18 @@ func TestNewReadPreference(t *testing.T) {
 		})
 
 		Convey("Specifying a read preference in the command line and URI should set it to the command line", func() {
-			rp := "{\"mode\": \"nearest\", \"tags\": {\"one\": \"two\"}}"
+			rp := "{\"mode\": \"nearest\", \"tagSets\": [{\"one\": \"two\"}], maxStalenessSeconds: 123}"
 			pref, err := NewReadPreference(rp, cs)
 			So(err, ShouldBeNil)
 			So(pref.Mode(), ShouldEqual, readpref.NearestMode)
+
 			tagSets := pref.TagSets()
 			So(len(tagSets), ShouldEqual, 1)
 			So(tagSets[0], ShouldResemble, tag.Set{tag.Tag{Name: "one", Value: "two"}})
+
+			maxStaleness, set := pref.MaxStaleness()
+			So(set, ShouldBeTrue)
+			So(maxStaleness, ShouldEqual, 123 * time.Second)
 		})
 	})
 }
