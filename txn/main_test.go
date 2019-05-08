8e6897b69c3a62ec6408920b9b33af1adb9de065
David Golden <xdg@xdg.me>
2019-05-08 16:36:40 -0400

    TOOLS-2291 Implement txn oplog meta parser

---
+++ txn/main_test.go
@@ -0,0 +1,89 @@
+package txn
+
+import (
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"testing"
+
+	"github.com/mongodb/mongo-tools-common/db"
+	"go.mongodb.org/mongo-driver/bson"
+)
+
+const (
+	OplogEntriesFile = "testdata/oplog_entries.json"
+)
+
+func readTestData() (bson.Raw, error) {
+	b, err := ioutil.ReadFile(OplogEntriesFile)
+	if err != nil {
+		return nil, fmt.Errorf("Couldn't load %s: %v", OplogEntriesFile, err)
+	}
+	var data bson.Raw
+	err = bson.UnmarshalExtJSON(b, false, &data)
+	if err != nil {
+		return nil, fmt.Errorf("Couldn't decode JSON: %v", err)
+	}
+	return data, nil
+}
+
+func getOpsForCase(name string, data bson.Raw) ([]db.Oplog, error) {
+	rawArray, err := data.LookupErr(name)
+	if err != nil {
+		return nil, fmt.Errorf("Couldn't find ops for case %s: %v", name, err)
+	}
+	rawOps, err := rawArray.Array().Elements()
+	if err != nil {
+		return nil, fmt.Errorf("Couldn't extract array elements for case %s: %v", name, err)
+	}
+	ops := make([]db.Oplog, len(rawOps))
+	for i, e := range rawOps {
+		err := e.Value().Unmarshal(&ops[i])
+		if err != nil {
+			return nil, fmt.Errorf("Couldn't unmarshal op %d for case %s: %v", i, name, err)
+		}
+	}
+	return ops, nil
+}
+
+type TestData struct {
+	name         string
+	entryCount   int
+	innerOpCount int
+	notTxn       bool
+	commits      bool
+	aborts       bool
+	ops          []db.Oplog
+}
+
+var testCases = []*TestData{
+	{name: "not transaction", entryCount: 1, notTxn: true},
+	{name: "applyops not transaction", entryCount: 1, notTxn: true},
+	{name: "small, unprepared", entryCount: 1, innerOpCount: 3, commits: true},
+	{name: "large, unprepared", entryCount: 3, innerOpCount: 10, commits: true},
+	{name: "small, prepared, committed", entryCount: 2, innerOpCount: 3, commits: true},
+	{name: "small, prepared, aborted", entryCount: 2, innerOpCount: 3, aborts: true},
+	{name: "large, prepared, committed", entryCount: 4, innerOpCount: 10, commits: true},
+	{name: "large, prepared, aborted", entryCount: 4, innerOpCount: 10, aborts: true},
+}
+
+func TestMain(m *testing.M) {
+	flag.Parse()
+
+	data, err := readTestData()
+	if err != nil {
+		panic(err)
+	}
+
+	for i, c := range testCases {
+		ops, err := getOpsForCase(c.name, data)
+		if err != nil {
+			panic(err)
+		}
+		// c is a copy and we want to change the original
+		testCases[i].ops = ops
+	}
+
+	os.Exit(m.Run())
+}
