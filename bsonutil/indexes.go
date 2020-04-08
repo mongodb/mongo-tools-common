dc0abbe5082abdf5b93fda989c12c63f691d7d6e
Tim Fogarty <tim.fogarty@mongodb.com>
2020-04-08 13:34:32 +0200

    Add ConvertLegacyIndexOptions()

--- bsonutil/indexes.go
+++ bsonutil/indexes.go
@@ -6,6 +6,33 @@ import (
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
+// validIndexOptions are taken from https://github.com/mongodb/mongo/blob/master/src/mongo/db/index/index_descriptor.h
+var validIndexOptions = map[string]bool{
+	"2dsphereIndexVersion":    true,
+	"background":              true,
+	"bits":                    true,
+	"bucketSize":              true,
+	"coarsestIndexedLevel":    true,
+	"collation":               true,
+	"default_language":        true,
+	"expireAfterSeconds":      true,
+	"finestIndexedLevel":      true,
+	"key":                     true,
+	"language_override":       true,
+	"max":                     true,
+	"min":                     true,
+	"name":                    true,
+	"ns":                      true,
+	"partialFilterExpression": true,
+	"sparse":                  true,
+	"storageEngine":           true,
+	"textIndexVersion":        true,
+	"unique":                  true,
+	"v":                       true,
+	"weights":                 true,
+	"wildcardProjection":      true,
+}
+
 // ConvertLegacyIndexKeys transforms the values of index definitions pre 3.4 into
 // the stricter index definitions of 3.4+. Prior to 3.4, any value in an index key
 // that isn't a negative number or that isn't a string is treated as 1.
@@ -53,6 +80,26 @@ func ConvertLegacyIndexKeys(indexKey bson.D, ns string) {
 		newJSONString := CreateExtJSONString(indexKey)
 		log.Logvf(log.Always, "convertLegacyIndexes: converted index values '%s' to '%s' on collection '%s'",
 			originalJSONString, newJSONString, ns)
+	}
+}
+
+// ConvertLegacyIndexOptions removes options that don't match a known list of index options.
+// It is preferable to use the ignoreUnknownIndexOptions on the createIndex command to
+// force the server to do this task. But that option was only added in 4.1.9. So for
+// pre 3.4 indexes being added to servers 3.4 - 4.2, we must strip the options in the client.
+func ConvertLegacyIndexOptions(indexOptions bson.M) {
+	var converted bool
+	originalJSONString := CreateExtJSONString(indexOptions)
+	for key := range indexOptions {
+		if _, ok := validIndexOptions[key]; !ok {
+			delete(indexOptions, key)
+			converted = true
+		}
+	}
+	if converted {
+		newJSONString := CreateExtJSONString(indexOptions)
+		log.Logvf(log.Always, "convertLegacyIndexes: converted index options '%s' to '%s'",
+			originalJSONString, newJSONString)
 	}
 }
 
