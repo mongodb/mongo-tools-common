ce3ee9a1300b1c86069d0fc8e1f1ed3369e82069
huan-Mongo <huan.li@mongodb.com>
2020-04-21 09:47:54 -0400

    MGOMIRROR-339: add function to convert legacy v1 indexes from createIndex Op

--- bsonutil/indexes.go
+++ bsonutil/indexes.go
@@ -103,6 +103,28 @@ func ConvertLegacyIndexOptions(indexOptions bson.M) {
 	}
 }
 
+// ConvertLegacyIndexOptions removes options that don't match a known list of index options.
+// It is preferable to use the ignoreUnknownIndexOptions on the createIndex command to
+// force the server to do this task. But that option was only added in 4.1.9. So for
+// pre 3.4 indexes being added to servers 3.4 - 4.2, we must strip the options in the client.
+func ConvertLegacyIndexOptionsFromOp(indexOptions *bson.D) {
+	var converted bool
+	doc := *indexOptions
+	originalJSONString := CreateExtJSONString(indexOptions)
+	for i, elem := range *indexOptions {
+		if _, ok := validIndexOptions[elem.Key]; !ok && elem.Key != "createIndexes" {
+			// Remove this key.
+			*indexOptions = append(doc[:i], doc[i+1:]...)
+			converted = true
+		}
+	}
+	if converted {
+		newJSONString := CreateExtJSONString(indexOptions)
+		log.Logvf(log.Always, "ConvertLegacyIndexOptionsFromOp: converted index options '%s' to '%s'",
+			originalJSONString, newJSONString)
+	}
+}
+
 // CreateExtJSONString stringifies doc as Extended JSON. It does not error
 // if it's unable to marshal the doc to JSON.
 func CreateExtJSONString(doc interface{}) string {
