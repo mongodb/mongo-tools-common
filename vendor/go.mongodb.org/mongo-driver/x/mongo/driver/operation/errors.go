b582a9e4f46ef979d07b01f7cb46968176d35c8d
Matthew Chiaravalloti <matthew.chiaravalloti@mongodb.com>
2020-07-13 10:40:05 -0400

    TOOLS-2569: Update go driver dependency to 1.4.0

--- vendor/go.mongodb.org/mongo-driver/x/mongo/driver/operation/errors.go
+++ vendor/go.mongodb.org/mongo-driver/x/mongo/driver/operation/errors.go
@@ -4,7 +4,10 @@
 // not use this file except in compliance with the License. You may obtain
 // a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
 
-package version // import "go.mongodb.org/mongo-driver/version"
+package operation
 
-// Driver is the current version of the driver.
-var Driver = "v1.4.0-rc0"
+import "errors"
+
+var (
+	errUnacknowledgedHint = errors.New("the 'hint' command parameter cannot be used with unacknowledged writes")
+)
