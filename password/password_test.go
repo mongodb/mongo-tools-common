fe25cc15378c27eaf4a080ff52ccb8752bb7d7af
Varsha Subrahmanyam <varsha.subrahmanyam@mongodb.com>
2020-08-20 12:25:26 -0500

    add password test

---
+++ password/password_test.go
@@ -0,0 +1,25 @@
+package password
+
+import (
+	"bytes"
+	"github.com/mongodb/mongo-tools-common/testtype"
+	. "github.com/smartystreets/goconvey/convey"
+	"testing"
+)
+
+const (
+	testPwd = "test_pwd"
+)
+func TestPasswordFromNonTerminal(t *testing.T) {
+	testtype.SkipUnlessTestType(t, testtype.UnitTestType)
+	Convey("stdin is not a terminal", t, func() {
+		var b bytes.Buffer
+
+		b.WriteString(testPwd)
+		reader := bytes.NewReader(b.Bytes())
+
+		pass, err := readPassNonInteractively(reader)
+		So(err, ShouldBeNil)
+		So(pass, ShouldEqual, testPwd)
+	})
+}
