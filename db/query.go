92985e4543ab47a2c4cfa17215a812b637f2baed
huan-Mongo <huan.li@mongodb.com>
2021-01-08 11:26:56 -0500

    fix lint

--- db/query.go
+++ db/query.go
@@ -20,7 +20,7 @@ func (q *DeferredQuery) Count() (int, error) {
 
 	if q.Filter == nil {
 		emptyFilter = true
-	} else if val, ok := q.Filter.(bson.D); ok && (val == nil || len(val.Map()) == 0)  {
+	} else if val, ok := q.Filter.(bson.D); ok && (val == nil || len(val.Map()) == 0) {
 		emptyFilter = true
 	} else if val, ok := q.Filter.(bson.M); ok && (val == nil || len(val) == 0) {
 		emptyFilter = true
@@ -30,11 +30,11 @@ func (q *DeferredQuery) Count() (int, error) {
 		opt := mopt.EstimatedDocumentCount()
 		c, err := q.Coll.EstimatedDocumentCount(nil, opt)
 		return int(c), err
-	} else {
-		opt := mopt.Count()
-		c, err := q.Coll.CountDocuments(nil, q.Filter, opt)
-		return int(c), err
 	}
+
+	opt := mopt.Count()
+	c, err := q.Coll.CountDocuments(nil, q.Filter, opt)
+	return int(c), err
 }
 
 // Iter executes a find query and returns a cursor.
